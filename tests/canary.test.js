eccee54a0c599b2e3bf0aac4e215d385c7285f1f
David <mullerlight73@hotmail.com>
2020-06-07 17:15:57 +0800

    first commit

---
+++ tests/canary.test.js
@@ -0,0 +1,6 @@
+describe("the canary meets gas", () => {
+  it("should not kill the canary", () => {
+    expect.assertions(1);
+    expect(true).toBe(true);
+  });
+});
---
+++ webpack/production.webpack.js
@@ -0,0 +1,33 @@
+const path = require("path");
+// eslint-disable-next-line import/no-extraneous-dependencies
+const webpack = require("webpack");
+// eslint-disable-next-line import/no-extraneous-dependencies
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = () => {
+  return {
+    mode: "production",
+    entry: "./src/index.js",
+    output: {
+      filename: `${process.env.ENV}-server.js`,
+      path: path.resolve("dist"),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /[\\/]node_modules[\\/]/,
+          use: {
+            loader: "babel-loader",
+          },
+        },
+      ],
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new webpack.EnvironmentPlugin({
+        ENV: process.env.ENV,
+      }),
+    ],
+  };
+};
